5e0141a66bbdeaea8c81b6af960715c8b48e3130
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-01-26 00:30:11 +0000

    Refactor tests to check printed receipts

--- test/sales_taxes_test.rb
+++ test/sales_taxes_test.rb
@@ -14,7 +14,7 @@ class SalesTaxesTest < Test::Unit::TestCase
     @items << Item.new(1, 'chocolate bar', :food, 0.85)
 
     result = SalesTaxes.new(@items).run
-    expected_output = { taxes: 1.50, total: 29.83 }
+    expected_output = "1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.5\nTotal: 29.83"
 
     assert_equal @items[0].total_price, 12.49
     assert_equal @items[1].total_price, 16.49
@@ -27,7 +27,7 @@ class SalesTaxesTest < Test::Unit::TestCase
     @items << Item.new(1, 'bottle of perfume', :cosmetics, 47.50, true)
 
     result = SalesTaxes.new(@items).run
-    expected_output = { taxes: 7.65, total: 65.15 }
+    expected_output = "1 imported box of chocolates: 10.5\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\nTotal: 65.15"
 
     assert_equal @items[0].total_price, 10.50
     assert_equal @items[1].total_price, 54.65
@@ -38,10 +38,10 @@ class SalesTaxesTest < Test::Unit::TestCase
     @items << Item.new(1, 'bottle of perfume', :cosmetics, 27.99, true)
     @items << Item.new(1, 'bottle of perfume', :cosmetics, 18.99)
     @items << Item.new(1, 'packet of headache pills', :medical, 9.75)
-    @items << Item.new(1, 'box of imported chocolates', :food, 11.25, true)
+    @items << Item.new(1, 'box of chocolates', :food, 11.25, true)
 
     result = SalesTaxes.new(@items).run
-    expected_output = { taxes: 6.70, total: 74.68 }
+    expected_output = "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n1 imported box of chocolates: 11.85\nSales Taxes: 6.7\nTotal: 74.68"
 
     assert_equal @items[0].total_price, 32.19
     assert_equal @items[1].total_price, 20.89
@@ -55,7 +55,7 @@ class SalesTaxesTest < Test::Unit::TestCase
     @items << Item.new(1, 'bottle of perfume', :cosmetics, 47.50, true)
 
     result = SalesTaxes.new(@items).run
-    expected_output = { taxes: 8.15, total: 75.65 }
+    expected_output = "2 imported box of chocolates: 21.0\n1 imported bottle of perfume: 54.65\nSales Taxes: 8.15\nTotal: 75.65"
 
     assert_equal @items[0].total_price, 21.00
     assert_equal @items[1].total_price, 54.65
@@ -66,10 +66,10 @@ class SalesTaxesTest < Test::Unit::TestCase
     @items << Item.new(2, 'bottle of perfume', :cosmetics, 27.99, true)
     @items << Item.new(1, 'bottle of perfume', :cosmetics, 18.99)
     @items << Item.new(3, 'packet of headache pills', :medical, 9.75)
-    @items << Item.new(4, 'box of imported chocolates', :food, 11.25, true)
+    @items << Item.new(4, 'box of chocolates', :food, 11.25, true)
 
     result = SalesTaxes.new(@items).run
-    expected_output = { taxes: 12.7, total: 161.92 }
+    expected_output = "2 imported bottle of perfume: 64.38\n1 bottle of perfume: 20.89\n3 packet of headache pills: 29.25\n4 imported box of chocolates: 47.4\nSales Taxes: 12.7\nTotal: 161.92"
 
     assert_equal @items[0].total_price, 64.38
     assert_equal @items[1].total_price, 20.89
