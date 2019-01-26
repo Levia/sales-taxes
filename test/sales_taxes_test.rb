88707184a26d2676ff3135d79cef0244f134acd8
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-01-26 00:04:55 +0000

    Add more complex tests with multiple amounts per product

--- test/sales_taxes_test.rb
+++ test/sales_taxes_test.rb
@@ -49,5 +49,33 @@ class SalesTaxesTest < Test::Unit::TestCase
     assert_equal @items[3].total_price, 11.85
     assert_equal result, expected_output
   end
+
+  def test_input_with_multiple_occurrences_of_same_product
+    @items << Item.new(2, 'box of chocolates', :food, 10.00, true)
+    @items << Item.new(1, 'bottle of perfume', :cosmetics, 47.50, true)
+
+    result = SalesTaxes.new(@items).run
+    expected_output = { taxes: 8.15, total: 75.65 }
+
+    assert_equal @items[0].total_price, 21.00
+    assert_equal @items[1].total_price, 54.65
+    assert_equal result, expected_output
+  end
+
+  def test_more_complex_input
+    @items << Item.new(2, 'bottle of perfume', :cosmetics, 27.99, true)
+    @items << Item.new(1, 'bottle of perfume', :cosmetics, 18.99)
+    @items << Item.new(3, 'packet of headache pills', :medical, 9.75)
+    @items << Item.new(4, 'box of imported chocolates', :food, 11.25, true)
+
+    result = SalesTaxes.new(@items).run
+    expected_output = { taxes: 12.7, total: 161.92 }
+
+    assert_equal @items[0].total_price, 64.38
+    assert_equal @items[1].total_price, 20.89
+    assert_equal @items[2].total_price, 29.25
+    assert_equal @items[3].total_price, 47.4
+    assert_equal result, expected_output
+  end
 end
 
